6a1be296c1dd3a31c380606d461e8ee79277fcdc
juanito42 <131249721+juanito42@users.noreply.github.com>
2023-05-13 11:48:46 +0200

    Añadida Clase Gato

--- src/animales1/Gato.java
+++ src/animales1/Gato.java
@@ -1,13 +1,114 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package animales1;
 
-/**
- *
- * @author juanc
- */
-public class Gato {
-    
+import java.util.Objects;
+
+public class Gato extends Animal1 {
+
+    private String raza;
+
+    public Gato(String codigo, String fechaNacimiento, char sexo, double peso, String raza) throws IllegalArgumentException {
+        super(codigo, fechaNacimiento, sexo, peso);
+
+        if (codigo.charAt(0) != 'g' || "".equals(raza)) {
+            throw new IllegalArgumentException();
+        } else {
+            this.codigo = codigo;
+            this.raza = raza;
+        }
+
+    }
+
+    public Gato(Gato otroGato) throws IllegalArgumentException {
+        super(otroGato);
+    }
+
+    public Gato(String raza, Animal1 OtroAnimal) {
+        super(OtroAnimal);
+        this.raza = raza;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        if ("".equals(raza)) {
+            throw new IllegalArgumentException();
+        } else {
+            this.raza = raza;
+        }
+    }
+
+    @Override
+    public void setCodigo(String codigo) throws IllegalArgumentException {
+        if (codigo.charAt(0) != 'p') {
+            throw new IllegalArgumentException();
+        } else {
+            super.setCodigo(codigo);
+        }
+    }
+
+    public String cazar() {
+        return "Me encanta cazar bichejos";
+    }
+
+    @Override
+    public String hacerSonido() {
+        return "Miau";
+    }
+
+    @Override
+    public String alegrarse() {
+        return "Ronroneo y me froto contra tus piernas";
+    }
+
+    @Override
+    public String enfadarse() {
+        return "Me bufo y saco las u??as";
+    }
+
+    @Override
+    public String queSoy() {
+        return "Soy un gato";
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public void setFechaNacimiento(String fechaNacimiento) throws IllegalArgumentException {
+        super.setFechaNacimiento(fechaNacimiento);
+    }
+
+    @Override
+    public String getFechaNacimiento() {
+        return super.getFechaNacimiento();
+    }
+
+    @Override
+    public double getPeso() {
+        return super.getPeso();
+    }
+
+    @Override
+    public char getSexo() {
+        return super.getSexo();
+    }
+
+    @Override
+    public String toString() {
+        return "Gato{Animal{codigo=" + getCodigo() + ", fechaNacimiento=" + getFechaNacimiento() + ", sexo=" + getSexo() + ", peso=" + getPeso() + "}raza=" + getRaza() + '}';
+    }
 }
